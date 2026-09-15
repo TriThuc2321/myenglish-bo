ad2764946d00a3e2e3af42b5eb9d02f3f2b15569
Thuc Tran <trithuc23232@gmail.com>
2026-09-16 00:17:35 +0700

    style: config theme

--- next.config.ts
+++ next.config.ts
@@ -24,6 +24,7 @@ const nextConfig: NextConfig = {
       },
     },
   },
+  allowedDevOrigins: ['backoffice.myenglish.com'],
 };
 
 const withBundleAnalyzer = bundleAnalyzer({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
 import { Button, Input } from '@heroui/react';
 
+import ThemeSwitch from '@/components/layouts/switchTheme';
+
 export default function Home() {
   return (
     <div>
       <Button>My Button</Button>
       <Input placeholder="Hello" />
+      <ThemeSwitch />
     </div>
   );
 }
