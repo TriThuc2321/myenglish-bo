bcb6f76ef047e7db04932b1b26bace93bbce85e1
Thuc Tran <trithuc23232@gmail.com>
2026-09-15 17:28:43 +0700

    style: hero ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Button>My Button</Button>
-      <Input placeholder="Hellow" />
+      <Input placeholder="Hello" />
     </div>
   );
 }
